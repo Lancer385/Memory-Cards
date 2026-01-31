7ae0203eb12f80c2c0a873365d656e25ba5fcaf7
Lancer385 <ahmedkhalaf1234123@gmail.com>
2026-01-31 14:30:43 +0200

    feat: handle api wait time and implement shuffling - add: isFinished useState to confirm when the data is fetched - add: shuffle function that uses Fisher–Yates algorithm to shuffle the cards - refactor: small change to the fetch https link.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,23 +1,35 @@
 import { fetchImages } from "../modules/fetchImages"
-import { useState, useEffect } from "react"
+import { useState, useEffect} from "react"
 import Card from "./Card";
 
 function Cards(){
     const [data, setData] = useState([]);
-    
- useEffect(() => {
-    let isMounted = true;
-    const getImages = async () => {
-        const images = await fetchImages();
-        if (isMounted){
-            setData(images);
-        }
+    const [isFinished, setFetchStatus] = useState(false)
+    function handleShuffle(){
+        setData(prev => shuffle(prev));
+    }
+    useEffect(() => {
+        console.log(isFinished)
+        let isMounted = true;
+        const getImages = async () => {
+            const images = await fetchImages();
+            if (isMounted){
+                setData(images);
+                setFetchStatus(true);
+            }
     }
     getImages();
     return () => {
         isMounted = false;
     }
 }, []);
+
+    if (!isFinished){
+        return (
+        <><h1>STAND BY...</h1></>
+        )
+    }
+    else {
 
 
     return (
@@ -27,11 +39,24 @@ function Cards(){
                     key = {value.id}
                     url = {value.url}
                     name = {value.breeds[0].name}
+                    handleShuffle={handleShuffle}
                 />
             ))} 
         </>
     )
+    }
 }
 
+function shuffle(arr){
+    let newArr = [...arr]
+    let curr = newArr.length;
 
+    while (curr !== 0){
+        let random = Math.floor(Math.random() * curr)
+        curr = curr - 1;
+
+        [newArr[curr], newArr[random]] = [newArr[random], newArr[curr]];
+    }
+    return newArr;
+}
 export default Cards
